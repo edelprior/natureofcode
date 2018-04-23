49f37a522f31e1ae6721ada6789ca59c38cde661
edelprior <edelprior@hotmail.com>
2018-04-23 20:38:23 +0100

    random

--- RecursiveFractalTreesPartTwo/js/branch.js
+++ RecursiveFractalTreesPartTwo/js/branch.js
@@ -1,18 +1,29 @@
 function Branch(begin, end) {
   this.begin = begin;
   this.end = end;
+  this.finished = false;
 
 
 this.show = function() {
-  stroke(255);
+  stroke(0);
   line(this.begin.x, this.begin.y, this.end.x, this.end.y);
 }
 
-this.branch = function() {
+this.branchA = function() {
+
   var dir = p5.Vector.sub(this.end, this.begin);
-  dir.rotate(PI / 4);
+  dir.rotate(PI / 6);
+  dir.mult(0.75);
   var newEnd = p5.Vector.add(this.end, dir);
-  var right = new Branch(this.end, newEnd);
-  return right;
+  var b = new Branch(this.end, newEnd);
+  return b;
+}
+this.branchB = function() {
+  var dir = p5.Vector.sub(this.end, this.begin);
+  dir.rotate(-PI / 6);
+  dir.mult(0.75);
+  var newEnd = p5.Vector.add(this.end, dir);
+  var b = new Branch(this.end, newEnd);
+  return b;
 }
 }
--- RecursiveFractalTreesPartTwo/js/sketch.js
+++ RecursiveFractalTreesPartTwo/js/sketch.js
@@ -1,4 +1,6 @@
 var tree = [];
+var leaves = [];
+var count = 0;
 
 function setup() {
   createCanvas(400,400);
@@ -7,15 +9,40 @@ function setup() {
   var root = new Branch(a, b);
 
   tree[0] = root;
+}
+function mousePressed() {
 
-  var newBranch = root.branch();
-  tree[1] = newBranch;
+  for (var i = tree.length-1; i >= 0; i--) {
+    if(!tree[i].finished){
+    tree.push(tree[i].branchA());
+    tree.push(tree[i].branchB());
+  }
+  tree[i].finished = true;
+  }
+  count ++;
+
+  if (count === 6) {
+    for (var i =0; i < tree.length; i++){
+      if(!tree[i].finished) {
+        var leaf = tree[i].end.copy();
+        leaves.push(leaf);
+
+      }
+
+    }
+  }
 }
 
 function draw() {
-  background(65);
+  background(255);
   for (var i = 0; i < tree.length; i ++) {
   tree[i].show();
 
   }
+
+  for (var i = 0; i < leaves.length; i ++) {
+  fill(0,100,0);
+  noStroke();
+  ellipse(leaves[i].x, leaves[i].y, random(8));
+}
 }
